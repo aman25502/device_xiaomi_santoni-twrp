80969a76a04e338b4bc2127fbc83f010d0117c8a
aman25502 <65896145+aman25502@users.noreply.github.com>
2020-07-01 18:38:03 +0530

    Update omni_santoni.mk

--- omni_santoni.mk
+++ omni_santoni.mk
@@ -1,9 +1,6 @@
 #
 # Copyright (C) 2017 The Android Open Source Project
 #
-#
-# Copyright (C) 2017 The Android Open Source Project
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -22,9 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from our custom product configuration.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
